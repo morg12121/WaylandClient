e606def6e46e4f42cf2ffd3c515a7af96f8c5d68
Felix ItsTheIntrovertedGuy <max0018@t-online.de>
2019-05-16 19:57:21 +0200

    Window finally opens, finally know where the header files come from - but stuff is still inherently borked

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <unistd.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
 #include <wayland-client.h>
+//#include <wayland-client-protocol.h>
+#include "xdg-shell-protocol.c"
+#include "xdg-shell-client-protocol.h"
 
 #define RESOLUTION_WIDTH 1280
 #define RESOLUTION_HEIGHT 720
@@ -13,26 +17,36 @@
 struct wl_display *Display = 0;
 struct wl_compositor *Compositor = 0;
 struct wl_shm *Shm = 0;
+struct xdg_wm_base *Base;
+struct xdg_toplevel *XdgToplevel = 0;
+static int32_t GlobalRunning = 1;
 
 static void
 global_registry_handler(void *Data, struct wl_registry *Registry,
 						uint32_t Id,
 						const char *Interface, uint32_t Version)
 {
-	printf("Registry event for %s with ID %d received.\n", Interface, Id);
+	fprintf(stderr, "Registry event for %s with ID %d received.\n", Interface, Id);
 	// NOTE(Felix): Retreive global objects
-	if (strcmp(Interface, "wl_compositor") == 0)
+	if (strcmp(Interface, wl_compositor_interface.name) == 0)
 	{
 		Compositor = wl_registry_bind(Registry,
 									  Id,
 									  &wl_compositor_interface,
 									  1);
 	}
-	if (strcmp(Interface, "wl_shm") == 0)
+	else if (strcmp(Interface, wl_shm_interface.name) == 0)
 	{
 		Shm = wl_registry_bind(Registry,
 		                       Id,
 		                       &wl_shm_interface,
+		                       1);
+	}
+	else if (strcmp(Interface, xdg_wm_base_interface.name) == 0)
+	{
+		Base = wl_registry_bind(Registry,
+		                       Id,
+		                       &xdg_wm_base_interface,
 		                       1);
 	}
 }
@@ -41,12 +55,39 @@ static void
 global_registry_remover(void *Data, struct wl_registry *Registry,
 						uint32_t Id)
 {
-	printf("Registry remove call received.\n");
+	fprintf(stderr, "Registry remove call received.\n");
 }
 
 static const struct wl_registry_listener GlobalRegistryListener = {
 	global_registry_handler,
 	global_registry_remover
+};
+
+static void xdg_surface_handle_configure (void *data, 
+										  struct xdg_surface *xdg_surface,
+										  uint32_t serial)
+{
+	xdg_surface_ack_configure(xdg_surface, serial);
+}
+
+static const struct xdg_surface_listener  xdg_surface_listener = {
+	 .configure = xdg_surface_handle_configure
+};
+
+static void
+noop()
+{
+}
+
+static void xdg_toplevel_handle_close(void *data,
+									  struct xdg_toplevel *xdg_toplevel)
+{
+	GlobalRunning = 0;
+}
+
+static const struct xdg_toplevel_listener xdg_toplevel_listener = {
+	.configure = noop,
+	.close = xdg_toplevel_handle_close
 };
 
 int 
@@ -55,7 +96,7 @@ main(void)
 	Display = wl_display_connect(0);
 	if (!Display) 
 	{
-		printf("Could not connect to Wayland display.\n");
+		fprintf(stderr, "Could not connect to Wayland display.\n");
 		return (-1);
 	}
 
@@ -66,54 +107,76 @@ main(void)
 
 	if (!Compositor)
 	{
-		printf("Cannot find compositor.\n");
+		fprintf(stderr, "Cannot find compositor.\n");
 		return (-1);
 	}
-	printf("Found compositor.\n");
+	fprintf(stderr, "Found compositor.\n");
 
 	if (!Shm)
 	{
-		printf("Cannot find global shared memory object.\n");
+		fprintf(stderr, "Cannot find global shared memory object.\n");
 		return (-1);
 	}
-	printf("Found global shared memory object.\n");
+	fprintf(stderr, "Found global shared memory object.\n");
+
+	if (!Base)
+	{
+		fprintf(stderr, "Cannot find global xdg_wm_base object.\n");
+		return (-1);
+	}
+	fprintf(stderr, "Found global xdg_wm_base object.\n");
 
 	// NOTE(Felix): Create surface - is used to draw stuff on it
 	struct wl_surface *Surface = wl_compositor_create_surface(Compositor);
 	if (!Surface)
 	{
-		printf("Could not create surface.\n");
+		fprintf(stderr, "Could not create surface.\n");
 		return (-1);
 	}
-	printf("Created surface.\n");
+	fprintf(stderr, "Created surface.\n");
 
 	// NOTE(Felix): Create Region
-	struct wl_region *Region = wl_compositor_create_region(Compositor);
+	/*struct wl_region *Region = wl_compositor_create_region(Compositor);
 	if (!Region)
 	{
-		printf("Could not create Region.\n");
+		fprintf(stderr, "Could not create Region.\n");
 		return (-1);
 	}
-	printf("Created region.\n");
+	fprintf(stderr, "Created region.\n");
 	wl_region_add(Region, 0, 0, RESOLUTION_WIDTH, RESOLUTION_HEIGHT);
 	wl_surface_set_opaque_region(Surface, Region);
-	wl_surface_set_input_region(Surface, Region);
+	wl_surface_set_input_region(Surface, Region);*/
+
+	uint64_t Size = sizeof(uint32_t)*RESOLUTION_WIDTH*RESOLUTION_HEIGHT;
+	uint64_t Stride = sizeof(uint32_t)*RESOLUTION_WIDTH;
 
 	// NOTE(Felix): Memory to map the pool to
 	char const *SharedMemoryName = "/MySharedMemory";
 	int Fd = shm_open(SharedMemoryName, O_RDWR|O_CREAT, S_IRWXU);
+	if (Fd == -1)
+	{
+		fprintf(stderr, "shm_open error.\n");
+	}
+
+	ftruncate(Fd, Size);
+	void *Data = mmap(0, Size, PROT_READ|PROT_WRITE, MAP_SHARED, Fd, 0);
+	if (Data == MAP_FAILED)
+	{
+		fprintf(stderr, "mmap failed.\n");
+		return (-1);
+	}
 	
 	// TODO(Felix): Get format with wl_shm_add_listener();
 	struct wl_shm_pool *MemoryPool = wl_shm_create_pool(Shm,
 														Fd,
-														sizeof(uint32_t)*RESOLUTION_WIDTH*RESOLUTION_HEIGHT);
+														Size);
 
 	if (!MemoryPool)
 	{
-		printf("Unable to create shared memory pool.\n");
+		fprintf(stderr, "Unable to create shared memory pool.\n");
 		return (-1);
 	}
-	printf("Created shared memory pool.\n");
+	fprintf(stderr, "Created shared memory pool.\n");
 
 	
 	// NOTE(Felix): Create wl_buffer
@@ -121,34 +184,59 @@ main(void)
 														 0,
 														 RESOLUTION_WIDTH,
 														 RESOLUTION_HEIGHT,
-														 sizeof(uint32_t)*RESOLUTION_WIDTH,
-														 WL_SHM_FORMAT_RGBA8888);
+														 Stride,
+														 WL_SHM_FORMAT_ARGB8888);
 
 	if (!Buffer)
 	{
-		printf("Unable to create buffer from shared memory.\n");
+		fprintf(stderr, "Unable to create buffer from shared memory.\n");
 		return (-1);
 	}
-	printf("Created buffer");
+	fprintf(stderr, "Created buffer\n");
 
 	
+	// NOTE(Felix): We are currently missing wl_shell. But we are also supposed to replace that with xdg_shell,
+	// but that documentation seems to be non-existant, even though the compositors are supposed to implement that.
+	// what.
+	struct xdg_surface *XdgSurface = xdg_wm_base_get_xdg_surface(Base, Surface);
+	XdgToplevel = xdg_surface_get_toplevel(XdgSurface);
+
+	xdg_surface_add_listener(XdgSurface, &xdg_surface_listener, 0);
+	xdg_toplevel_add_listener(XdgToplevel, &xdg_toplevel_listener, 0);
+
+	wl_surface_commit(Surface);
+	wl_display_roundtrip(Display);
+	
+
 	// NOTE(Felix): Attach buffer to surface
 	wl_surface_attach(Surface, Buffer,
 					  0, 0);
 	wl_surface_commit(Surface);
-		
-	// NOTE(Felix): We are currently missing wl_shell. But we are also supposed to replace that with xdg_shell,
-	// but that documentation seems to be non-existant, even though the compositors are supposed to implement that.
-	// what.
-	
-	//struct xdg_surface *XdgSurface = xdg_shell_get_xdg_surface(xdg_shell, Surface);
-	//struct xdg_surface *XdgSurface = xdg_wm_base_get_xdg_surface(xdg_wm_base, surface);
-	
-	
+
+	while (wl_display_dispatch(Display) != -1 && GlobalRunning) 
+	{
+		static uint32_t Color = 0;
+		for (uint32_t Y = 0; Y < RESOLUTION_HEIGHT; ++Y)
+		{
+			uint32_t *PixelY = Data + Y*Stride;
+			for (uint32_t X = 0; X < RESOLUTION_WIDTH; ++X)
+			{
+				uint32_t *PixelX = PixelY+X;
+				//*Pixel++ = 0xFF0000FF;
+				*PixelX = Color++ % 0x000000FF;
+			}
+		}
+	}
 
 	// NOTE(Felix): Process events - look into "wl_event_queue"
-	
-
+	//munmap(Data, Size);
+	//if (shm_unlink(SharedMemoryName) == -1)
+	//{
+	//	fprintf(stderr, "shm_unlink error.\n");
+	//}
+	xdg_toplevel_destroy(XdgToplevel);
+	xdg_surface_destroy(XdgSurface);
+	wl_surface_destroy(Surface);
 	wl_display_disconnect(Display);
 	return (0);
 }
--- makefile
+++ makefile
@@ -3,15 +3,22 @@ OPTIONS=-Wall -Werror
 DISABLEDWARNINGS=-Wno-unused-variable -Wno-unused-function
 OPTIMIZATIONS=-O0
 CODEFLAGS=
-LIBRARIES=-lwayland-client -lrt
-#LIBRARIES=-lLIBRARY
+LIBRARIES=-lrt
 #CODEFLAGS=-DFLAG
+
+WAYLAND_FLAGS = $(shell pkg-config wayland-client --cflags --libs)
+WAYLAND_PROTOCOLS_DIR = $(shell pkg-config wayland-protocols --variable=pkgdatadir)
+WAYLAND_SCANNER = $(shell pkg-config --variable=wayland_scanner wayland-scanner)
+XDG_SHELL_PROTOCOL = $(WAYLAND_PROTOCOLS_DIR)/stable/xdg-shell/xdg-shell.xml
+XDG_SHELL_FILES=xdg-shell-client-protocol.h xdg-shell-protocol.c
 
 FILE_MAIN=main.c
 FILE_OUTPUT=WaylandClient
 
 build:
-	gcc $(FILE_MAIN) -o $(FILE_OUTPUT) $(OPTIONS) $(DISABLEDWARNINGS) $(OPTIMIZATION) $(CODEFLAGS) $(LIBRARIES)
+	$(WAYLAND_SCANNER) client-header $(XDG_SHELL_PROTOCOL) xdg-shell-client-protocol.h
+	$(WAYLAND_SCANNER) private-code $(XDG_SHELL_PROTOCOL) xdg-shell-protocol.c
+	gcc $(FILE_MAIN) -o $(FILE_OUTPUT) $(OPTIONS) $(DISABLEDWARNINGS) $(OPTIMIZATION) $(CODEFLAGS) $(LIBRARIES) $(WAYLAND_FLAGS)
 	chmod +x $(FILE_OUTPUT)
 
 run:
